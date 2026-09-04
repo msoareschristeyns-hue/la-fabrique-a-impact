f0126bcb66a46abaf7bff57212212779601f1159
msoareschristeyns-hue <msoares.christeyns@gmail.com>
2026-09-05 01:14:44 +0200

    Remove public sidebar and keep application shell private only

--- components/SiteShell.tsx
+++ components/SiteShell.tsx
@@ -2,12 +2,11 @@
 import {ReactNode} from 'react';
 import {usePathname} from 'next/navigation';
 import AppShell from './AppShell';
-import PublicShell from './PublicShell';
 
 const publicRoutes=['/','/connexion','/inscription'];
 
 export default function SiteShell({children}:{children:ReactNode}){
   const pathname=usePathname();
   const isPublic=publicRoutes.some(route=>route==='/'?pathname==='/':pathname===route||pathname.startsWith(route+'/'));
-  return isPublic?<PublicShell>{children}</PublicShell>:<AppShell>{children}</AppShell>;
+  return isPublic?<>{children}</>:<AppShell>{children}</AppShell>;
 }
