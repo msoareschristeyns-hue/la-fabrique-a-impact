a4e7b5d02b172fe763361397621324d18c0e4cd2
msoareschristeyns-hue <msoares.christeyns@gmail.com>
2026-09-21 12:55:43 +0200

    feat: apply shared public shell to all public pages

--- components/SiteShell.tsx
+++ components/SiteShell.tsx
@@ -2,11 +2,12 @@
 import {ReactNode} from 'react';
 import {usePathname} from 'next/navigation';
 import AppShell from './AppShell';
+import PublicShell from './PublicShell';
 
 const publicRoutes=['/','/connexion','/inscription','/tarifs','/evaluation-rse'];
 
 export default function SiteShell({children}:{children:ReactNode}){
   const pathname=usePathname();
   const isPublic=publicRoutes.some(route=>route==='/'?pathname==='/':pathname===route||pathname.startsWith(route+'/'));
-  return isPublic?<>{children}</>:<AppShell>{children}</AppShell>;
+  return isPublic?<PublicShell>{children}</PublicShell>:<AppShell>{children}</AppShell>;
 }
